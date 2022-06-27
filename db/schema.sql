9b4df27a91b9b092fa371ea400d06495b28ae7e3
James G <stackz@protonmail.com>
2022-06-27 00:46:49 -0400

    created first 2 options to display correctly. Need to create the add department the rest

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,26 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT  UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,1) NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (deparment)
+    salary DECIMAL(10,1) UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE SET NULL
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    FOREIGN KEY (role)
-    REFERENCES  role(id)
-    ON DELETE SET NULL
+    role_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES  role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -12,6 +12,46 @@ const db = mysql.createConnection(
     console.log ('Connected to the company_db database')
 );
 
+const fn = {
+    viewAllDepartments() {
+        db.query('SELECT * FROM department', function (err, results) {
+            if (err) return console.error(err);
+            console.table(results);
+            return init();
+        });
+    },
+    viewAllRoles() {
+        db.query('SELECT title, role.id, department.name, salary FROM role JOIN department ON role.department_id = department.id', function (err, results) {
+            if (err) return console.error(err);
+            console.table(results);
+            return init();
+         })
+    },
+    viewAllEmployees() {
+        db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN employee manager on manager.id = employee.manager_id
+        INNER JOIN role ON (role.id = employee.role_id)
+        INNER JOIN department ON (department.id = role.department_id)
+        ORDER BY employee.id;`, function (err, results) {
+            if (err) return console.error(err);
+            console.table(results);
+            return init();
+         })
+    },
+    addNewDepartment() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "new department",
+                message: "Please enter the department name you want to add" 
+            }
+        ]) //need to find a way to pass answer into query to insert string into department_db
+    }
+    
+    
+};
+
 const init = () => {
     const choices = [
         { name: 'View all departments', value: 'viewAllDepartments' },
@@ -30,7 +70,7 @@ const init = () => {
             message: 'What option would you like to select?',
             choices,
         }
-    ]).then((answers) => finally[answers.query]());
+    ]).then((answers) => fn[answers.query]());
 };
 
 init();
